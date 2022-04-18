7a38a84dc22bcfaea3b2821e96aed3559a2e7149
fireball <fire@ball>
2022-04-18 18:17:07 -0500

    Fix the encoder script.

--- saveedit/encode.sh
+++ saveedit/encode.sh
@@ -4,7 +4,7 @@ dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 cd "$dir"
 
 if [ "${1:-x}" = "x" ]; then
-    printf "0fd%s" "$(sed $'s/||/\\\n/g' | base64)"
+    printf "0fd%s" "$(while read -r a; do printf "%s||" "$a"; done | base64)"
 else
-    printf "0fd%s" "$(sed $'s/||/\\\n/g' <"$1" | base64)"
+    printf "0fd%s" "$(cat <"$1" | while read -r a; do printf "%s||" "$a"; done | base64)"
 fi
