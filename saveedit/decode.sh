f7544f2d5a24275c7f7b0162865b4eb3b9a7a12c
fireball <fire@ball>
2022-04-20 12:07:47 -0500

    Remove directory changing from encode/decode scripts because it makes no sense.

--- saveedit/decode.sh
+++ saveedit/decode.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env sh
 set -o errexit -o noclobber
-dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
-cd "$dir"
 
 if [ "${1:-x}" = "x" ]; then
     tail -c +4 | base64 -d | sed $'s/||/\\\n/g'
--- saveedit/encode.sh
+++ saveedit/encode.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env sh
 set -o errexit -o noclobber
-dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
-cd "$dir"
 
 if [ "${1:-x}" = "x" ]; then
     printf "0fd%s" "$(while read -r a; do printf "%s||" "$a"; done | base64)"
